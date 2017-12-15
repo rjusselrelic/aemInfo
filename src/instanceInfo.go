abfdaf9976d5b73ac2a2e36db3d7ecfb0b37b644
Cloud User <ec2-user@ip-172-31-4-157.us-west-2.compute.internal>
2017-12-15 17:01:00 +0000

    Added Bundle information to Inventory

--- src/instanceInfo.go
+++ src/instanceInfo.go
@@ -1,14 +1,17 @@
 package main
+
 import (
 	sdkArgs "github.com/newrelic/infra-integrations-sdk/args"
 	"github.com/newrelic/infra-integrations-sdk/log"
 	"github.com/newrelic/infra-integrations-sdk/metric"
 	"github.com/newrelic/infra-integrations-sdk/sdk"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
-	//	"io/ioutil"
-	//"path/filepath"
+	"time"
 )
 
 type argumentList struct {
@@ -18,21 +21,43 @@ type argumentList struct {
 const (
 	integrationName    = "com.Ryan Jussel.instanceInfo"
 	integrationVersion = "0.1.3"
+	aemPath            = "/mnt/crx/"
 )
 
 var args argumentList
 
 func getCQadminPass(user string) string {
-	cmd := exec.Command("/bin/sh", "-c", "pass " + user)
-        output, err := cmd.CombinedOutput()
-        if err != nil {
-                return err
-        }
-        cqPass := strings.Trim(string(output), "\n")
+	cmd := exec.Command("/bin/sh", "-c", "pass "+user)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	cqPass := strings.Trim(string(output), "\n")
 	return cqPass
 
 }
 
+func getBundleTxt(user string) string {
+	passwd := getCQadminPass("CQ_Admin")
+	client := &http.Client{}
+	port := "4502"
+	if getAEMType(aemPath) == "publish" {
+		port = "4503"
+	}
+	url := "http://localhost:" + port + "/system/console/status-Bundlelist.txt"
+	log.Info("Requesting Bundlelist from " + url)
+	req, err := http.NewRequest("GET", url, nil)
+	req.SetBasicAuth(user, passwd)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Info("Failed to access " + url)
+		//log.Info(string(err))
+		return ""
+	}
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	s := string(bodyText)
+	return s
+}
 
 func getAEMType(path string) string {
 	aemType := ""
@@ -52,11 +77,10 @@ func populateInventory(inventory sdk.Inventory) error {
 	// Insert here the logic of your integration to get the inventory data
 	// Ex: inventory.SetItem("softwareVersion", "value", "1.0.1")
 	// --
-	aemPath := "/mnt/crx/"
+	//aemPath := "/mnt/crx/"
 	//aemType := ""
 
 	//Set AEM_TYPE
-
 	inventory.SetItem("AEM Type", "value", getAEMType(aemPath))
 
 	//Set OAK Version
@@ -66,21 +90,80 @@ func populateInventory(inventory sdk.Inventory) error {
 	cmd := exec.Command("/bin/sh", "-c", rawcmd)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
 	oakVersion := strings.Trim(string(output), "\n")
-	log.Debug(oakVersion)
+	log.Debug("Oak Version: ", oakVersion)
 	inventory.SetItem("Oak Version", "value", oakVersion)
 
 	pscmd := "ps -eo args | grep java | grep -v grep"
 	cmd = exec.Command("/bin/sh", "-c", pscmd)
 	output, err = cmd.CombinedOutput()
+	log.Debug("Java PS Output: %s", string(output))
+	log.Debug("output length: %d", len(output))
 	if err != nil {
-		return err
+		log.Info("Failing")
+		log.Info("Java Process not found.")
+		inventory.SetItem("Java Arguments", "value", "Process Not Found")
+	} else if len(output) > 0 {
+		procArgs := strings.Trim(string(output), "\n")
+		log.Info(procArgs)
+	} else {
 	}
-	procArgs := strings.Trim(string(output), "\n")
-	log.Info(procArgs)
-	inventory.SetItem("Java Arguments", "value", procArgs)
+
+	//Set Bundle Information
+	log.Debug("Starting Bundle Parsing...")
+	bundles := strings.Split(getBundleTxt("admin"), "\n")
+
+	t := time.Now()
+	currentTimeStamp := t.Format("Jan _2 15:04:05 2006 UTC")
+	log.Debug(t.Format("Jan _2 15:04:05 2006 UTC"))
+
+	// First we want to extract the data json using regex with a capture group.
+	parseBundle := regexp.MustCompile(`(\w+)\s\(([\d+\.]+)\)\s"(.*)"\s\[(\w+),\s\d+\]`)
+
+	for _, bundle := range bundles {
+		println(bundle)
+
+		// Parse Bundle for status and information
+		if parseBundle.MatchString(bundle) {
+			b := parseBundle.FindAllStringSubmatch(bundle, -1)[0]
+			pName := strings.Replace(b[3], "/", "|", -1)
+			log.Debug("Matched Bundle: ", b[1])
+			log.Debug("\tPrettyName: ", pName)
+			inventory.SetItem("Bundles/"+pName, "Symbolic Name", b[1])
+			log.Debug("\tRaw Name: ", b[1])
+			inventory.SetItem("Bundles/"+pName, "Version", b[1])
+			log.Debug("\tVersion: ", b[2])
+			inventory.SetItem("Bundles/"+pName, "Status", b[1])
+			log.Debug("\tStatus: ", b[4])
+			inventory.SetItem("Bundles/"+pName, "Last Updated", currentTimeStamp)
+
+		} else if regexp.MustCompile(`Status:.*`).MatchString(bundle) == true {
+			// Gather overall status
+			bundStats := strings.Split(strings.Trim(string(bundle), "Status: "), ",")
+			for _, stat := range bundStats {
+				//log.Debug(string(stat))
+				reg := regexp.MustCompile(`(?P<number>\d+)\sbundle[s]*\s(?P<name>[\w\s]+)`)
+				bundNames := reg.SubexpNames()
+				statuses := reg.FindAllStringSubmatch(stat, -1)[0]
+				md := map[string]string{}
+
+				for k, v := range statuses {
+					//log.Debug("%d. match='%s'\tname='%s'\n", k, v, bundNames[k])
+					md[bundNames[k]] = v
+					//log.Debug("Name: ",string(v[2]))
+
+				}
+				inventory.SetItem("Bundle Overview", md["name"], md["number"])
+				log.Debug("The Name is %s\n", md["name"])
+				log.Debug("The Number is %s\n", md["number"])
+			}
+		}
+
+	}
+
+	log.Info(getBundleTxt("admin"))
 
 	return nil
 }
@@ -93,10 +176,12 @@ func populateMetrics(ms *metric.MetricSet) error {
 }
 
 func main() {
+	log.Info("Starting Integration...")
 	integration, err := sdk.NewIntegration(integrationName, integrationVersion, &args)
 	fatalIfErr(err)
 
 	if args.All || args.Inventory {
+		log.Info("Running Inventory...")
 		fatalIfErr(populateInventory(integration.Inventory))
 	}
 
